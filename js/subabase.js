42ce5bd0db2f4d090a430a34604b6ce5ab1202ad
Trenton Guffey <tjguffey50@gmail.com>
2026-06-27 19:41:36 -0500

    update 1

--- js/subabase.js
+++ js/subabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabse/supabase-js@2/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
 const SUPABASE_URL = 'https://ksqsickcrkbemvducrge.supabase.co/rest/v1/'   
 const SUPABASE_KEY = 'sb_publishable_Gn_NxqVaN6Nb_88glMXrxA_U1ElUH3t'

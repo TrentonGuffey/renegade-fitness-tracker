cd6828e086e207456f91bdf1bf086358e84184f2
Trenton Guffey <tjguffey50@gmail.com>
2026-06-27 19:49:47 -0500

    update 3

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = 'https://ksqsickcrkbemvducrge.supabase.co/rest/v1/'   
+const SUPABASE_URL = 'https://ksqsickcrkbemvducrge.supabase.co'   
 const SUPABASE_KEY = 'sb_publishable_Gn_NxqVaN6Nb_88glMXrxA_U1ElUH3t'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
